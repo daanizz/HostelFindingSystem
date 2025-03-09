43e5ce922c952efb755c6d07e8275579e7c02872
danishz <danishvv1011@gmail.com>
2025-03-09 10:01:50 +0530

    cleared unecessary codes

--- controllers/HostelController.js
+++ controllers/HostelController.js
@@ -11,23 +11,3 @@ const addHostel = async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 };
-
-const getHostels = async (req, res) => {
-    try {
-        const hostels = await Hostel.find();
-        res.json(hostels);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-const deleteHostel = async (req, res) => {
-    try {
-        await Hostel.findByIdAndDelete(req.params.id);
-        res.json({ message: 'Hostel deleted' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-module.exports = { addHostel, getHostels, deleteHostel };
--- routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllReservations, deleteReservation } = require('../controllers/adminController');
-const { authMiddleware, adminMiddleware } = require('../config/auth');
-const router = express.Router();
-
-router.get('/reservations', authMiddleware, adminMiddleware, getAllReservations);
-router.delete('/reservations/:id', authMiddleware, adminMiddleware, deleteReservation);
-
-module.exports = router;
