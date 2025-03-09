dedfcb13e7e8cb5e7fa868ac6a20f6dce5b7ae47
danishz <danishvv1011@gmail.com>
2025-03-09 10:27:38 +0530

    cleared unwanted codes

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
-const authRoutes = require('./routes/authRoutes');
-const hostelRoutes = require('./routes/hostelRoutes');
-const reserveRoutes = require('./routes/reserveRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const { authMiddleware } = require('./config/auth');
+
 
 dotenv.config();
 connectDB();
@@ -16,11 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-app.use('/api/auth', authRoutes);
-app.use('/api/hostels', hostelRoutes);
-app.use('/api/reservations', reserveRoutes);
-app.use('/api/admin', adminRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
