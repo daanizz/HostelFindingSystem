0adeaeeb44f22293b057346cacb3811ed8837d13
danishz <danishvv1011@gmail.com>
2025-04-01 10:35:39 +0530

    simple commit

--- controllers/reserveController.js
+++ controllers/reserveController.js
@@ -8,6 +8,16 @@ exports.reserve= async (req,res) => {
             // return res.status(401).send('Please login to make a reservation');
         }
 
+        const existingReservation = await Reservation.findOne({
+            userId: req.session.user._id,
+            hostelId: req.body.hostelId
+        });
+
+        if (existingReservation) {
+            req.session.message = 'You already have a reservation in this hostel';
+            return res.redirect('/annex');
+        }
+
         const reservationID = Math.floor(100000 + Math.random() * 900000);
 
         const newres = new Reservation({
@@ -26,6 +36,7 @@ exports.reserve= async (req,res) => {
         }
         catch (error) {
             console.error('Reservation error:', error);
+            req.session.message = 'Error creating reservation';
             res.status(500).send('Error creating reservation');
         }
     }
